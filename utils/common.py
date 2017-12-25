82488c74f11c220d19630e3823a7d9b433d2278b
Ivan Khomyakov <khaxis@gmail.com>
2017-12-24 17:42:18 -0800

    basic api

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,17 @@
-from bson.objectid import ObjectId
+import json
+from datetime import datetime
+from bson import ObjectId
 
 def to_object_id(_id):
     if type(_id) != ObjectId:
         _id = ObjectId(_id)
     return _id
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
--- web/graph_list.py
+++ web/graph_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 from web.common import *
 from db.graph_collection_manager import GraphCollectionManager
+from utils.common import JSONEncoder
+
+graph_collection_manager = GraphCollectionManager()
 
 @app.route("/graphs/")
 def get_pool_list():
-    graph_collection_manager = GraphCollectionManager()
     return render_template('graph_list.html', graph_list=enumerate(graph_collection_manager.get_db_graphs()))
+
+@app.route('/plynx/api/v0/graphs', methods=['GET'])
+def get_tasks():
+    return JSONEncoder().encode({'data': graph_collection_manager.get_db_graphs(), 'status':'success'})
