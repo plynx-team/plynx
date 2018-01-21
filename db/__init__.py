1ea10c611716fd4451cf86f97a798ce9831a2fdb
Ivan Khomyakov <khaxis@gmail.com>
2018-01-21 11:52:58 -0800

    reorder deps

--- db/__init__.py
+++ db/__init__.py
@@ -1,12 +1,13 @@
 from .base_blocks import get_base_blocks
 from .db_object import DBObject
+from .input import Input, InputValue
+from .parameter import Parameter, ParameterWidget
+from .output import Output
 from .block import Block
 from .block_collection_manager import BlockCollectionManager
 from .graph import Graph
 from .graph_collection_manager import GraphCollectionManager
-from .input import Input, InputValue
-from .parameter import Parameter, ParameterWidget
-from .output import Output
+
 
 __all__ = [
     get_base_blocks,
