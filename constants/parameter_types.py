fa78069d3ecb3f0fd8ab49cae5e18227d82cb372
Ivan Khomyakov <khaxis@gmail.com>
2018-01-21 13:03:03 -0800

    make static methods

--- constants/parameter_types.py
+++ constants/parameter_types.py
@@ -1,12 +1,16 @@
-from abc import ABCMeta, abstractmethod
-
-
-class ParameterTypes(object):
+class ParameterTypes:
     STR = 'str'
 
-    @abstractmethod
+    @staticmethod
     def get_default_by_type(parameter_type):
         if parameter_type == ParameterTypes.STR:
             return ''
         else:
             return None
+
+    @staticmethod
+    def value_is_valid(value, parameter_type):
+        if parameter_type == ParameterTypes.STR:
+            return isinstance(value, basestring)
+        else:
+            return False
