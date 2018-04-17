e16ffeb865d313c681ef9c640e502171fdcb4ca5
Ivan Khomyakov <khaxis@gmail.com>
2018-04-16 20:49:51 -0700

    + auto layout

--- db/graph.py
+++ db/graph.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import deque, defaultdict
 from . import Block, File, DBObject, ValidationError
 from utils.db_connector import *
 from utils.common import to_object_id, ObjectId
@@ -118,6 +119,103 @@ class Graph(DBObject):
                     validation_code=ValidationCode.IN_DEPENDENTS,
                     children=violations
                     )
+
+    def arrange_auto_layout(self):
+        """Use heuristic to rearange nodes.
+        """
+        HEADER_HEIGHT = 23
+        DESCRIPTION_HEIGHT = 20
+        FOOTER_HEIGHT = 10
+        BORDERS_HEIGHT = 2
+        ITEM_HEIGHT = 20
+        SPACE_HEIGHT = 50
+        LEFT_PADDING = 30
+        TOP_PADDING = 80
+        LEVEL_WIDTH = 252
+        min_node_height = HEADER_HEIGHT + DESCRIPTION_HEIGHT + FOOTER_HEIGHT + BORDERS_HEIGHT
+
+        node_id_to_level = defaultdict(lambda: -1)
+        node_id_to_node = {}
+        queued_node_ids = set()
+
+        node_ids = set([node._id for node in self.blocks])
+        non_zero_block_ids = set()
+        for node in self.blocks:
+            node_id_to_node[node._id] = node
+            for input in node.inputs:
+                for value in input.values:
+                    non_zero_block_ids.add(to_object_id(value.block_id))
+
+        leaves = node_ids - non_zero_block_ids
+        to_visit = deque()
+        for leaf_id in leaves:
+            node_id_to_level[leaf_id] = 0
+            to_visit.append(leaf_id)
+
+        while to_visit:
+            node_id = to_visit.popleft()
+            node = node_id_to_node[node_id]
+            node_level = node_id_to_level[node_id]
+            for input in node.inputs:
+                for value in input.values:
+                    parent_node_id = to_object_id(value.block_id)
+                    parent_level = node_id_to_level[parent_node_id]
+                    node_id_to_level[parent_node_id] = max(node_level + 1, parent_level)
+                    if parent_node_id not in queued_node_ids:
+                        to_visit.append(parent_node_id)
+                        queued_node_ids.add(parent_node_id)
+
+        max_level = max(node_id_to_level.values())
+        level_to_node_ids = defaultdict(list)
+        row_heights = defaultdict(lambda: 0)
+
+        def get_index_helper(node, level):
+            if level < 0:
+                return 0
+            parent_node_ids = set()
+            for input in node.inputs:
+                for value in input.values:
+                    parent_node_ids.add(to_object_id(value.block_id))
+
+            for index, node_id in enumerate(level_to_node_ids[level]):
+                if node_id in parent_node_ids:
+                    return index
+            return len(level_to_node_ids[level])
+
+        def get_index(node, level):
+            return tuple(
+                [get_index_helper(node, lvl) for lvl in range(level - 1, -1 , -1)]
+                )
+
+        for node_id, level in node_id_to_level.items():
+            level_to_node_ids[level].append(node_id)
+
+        for level in range(max_level, -1, -1):
+            level_node_ids = level_to_node_ids[level]
+            index_to_node_id = []
+            for node_id in level_node_ids:
+                node = node_id_to_node[node_id]
+                index = get_index(node, level)
+                index_to_node_id.append((index, node_id))
+
+            index_to_node_id.sort()
+            level_to_node_ids[level] = [node_id for _, node_id in index_to_node_id]
+
+            for index, node_id in enumerate(level_to_node_ids[level]):
+                node = node_id_to_node[node_id]
+                node_height = min_node_height + ITEM_HEIGHT * max(len(node.inputs), len(node.outputs))
+                row_heights[index] = max(row_heights[index], node_height)
+
+        cum_heights = [0]
+        for index in range(len(row_heights)):
+            cum_heights.append(cum_heights[-1] + row_heights[index] + SPACE_HEIGHT)
+
+        for level in range(max_level, -1, -1):
+            level_node_ids = level_to_node_ids[level]
+            for index, node_id in enumerate(level_node_ids):
+                node = node_id_to_node[node_id]
+                node.x = LEFT_PADDING + (max_level - level) * LEVEL_WIDTH
+                node.y = TOP_PADDING + cum_heights[index]
 
     def __str__(self):
         return 'Graph(_id="{}", blocks={})'.format(self._id, [str(b) for b in self.blocks])
