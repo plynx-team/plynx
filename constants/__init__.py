3fb44c8486558ae171e6990830036a8033022d08
Ivan Khomyakov <khaxis@gmail.com>
2018-06-04 18:59:14 -0700

    remove file and block consts

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,6 +1,4 @@
-from .block_enums import BlockRunningStatus, BlockStatus, BlockPostAction, BlockPostStatus
 from .node_enums import NodeRunningStatus, NodeStatus, NodePostAction, NodePostStatus, JobReturnStatus
-from .file_enums import FileStatus, FilePostAction, FilePostStatus
 from .file_types import FileTypes
 from .graph_enums import GraphRunningStatus, GraphPostAction, GraphPostStatus
 from .parameter_types import ParameterTypes
@@ -8,14 +6,7 @@ from .resource_enums import ResourcePostStatus
 from .validation_enums import ValidationTargetType, ValidationCode
 
 __all__ = [
-    BlockRunningStatus,
-    BlockStatus,
-    BlockPostAction,
-    BlockPostStatus,
     JobReturnStatus,
-    FileStatus,
-    FilePostAction,
-    FilePostStatus,
     FileTypes,
     GraphRunningStatus,
     GraphPostAction,
