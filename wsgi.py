154dd4ad5b0bce3a7285add6b9ba36341fac1d9d
Ivan Khomyakov <khaxis@gmail.com>
2018-09-19 08:22:10 -0700

    fix web container script

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import os
 from web import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
