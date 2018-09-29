035613a1ecef44bd719d4b60cd746b362eeac663
Ivan Khomyakov <khaxis@gmail.com>
2018-09-28 22:48:21 -0700

    move docker-compose for integration test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,4 +4,4 @@ set -e
 
 source ./scripts/build_images.sh
 
-docker-compose -f tests/integration/docker-compose.yml up --abort-on-container-exit --scale workers=5
+docker-compose -f ./docker/compose-integration/docker-compose.yml up --abort-on-container-exit --scale workers=5
