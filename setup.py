c525795f0b142588d6c489d84151f3c0e9d33e98
Ivan Khomyakov <khaxis@gmail.com>
2018-11-13 21:57:58 -0800

    fix in setup py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
 
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Information Analysis',
