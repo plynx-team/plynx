cbee390673fd92ca8f21173cfb6c5bf2bd15946a
Ivan Khomyakov <khaxis@gmail.com>
2018-12-02 11:58:59 -0800

    do not build ui in tests

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -4,10 +4,10 @@ set -e
 
 source ./scripts/version.sh
 
-export VERSION=$(plynx-version)
-export IMAGES="base backend master worker test ui"
+VERSION=$(plynx-version)
+PLYNX_IMAGES=${PLYNX_IMAGES:="base backend master worker test ui"}
 
-for IMAGE in ${IMAGES}; do
+for IMAGE in ${PLYNX_IMAGES}; do
   docker build --rm -t khaxis/plynx:${IMAGE} -f docker/${IMAGE}/Dockerfile . ;
   docker tag khaxis/plynx:${IMAGE} khaxis/plynx_${VERSION}:${IMAGE};
 done
