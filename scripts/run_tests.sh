3192ab45b49353e98f9253d2c74e99dbb04b5c2a
Ivan Khomyakov <khaxis@gmail.com>
2019-04-02 08:04:18 -0700

    rm ui lint

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,8 +4,6 @@ set -e
 
 PLYNX_IMAGES="base backend master worker test"
 
-./ui/lint.sh
-
 source ./scripts/build_images.sh
 
 docker-compose -f ./docker/compose-integration/docker-compose.yml up --abort-on-container-exit --scale workers=5
