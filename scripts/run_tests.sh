3227a0195f32ee75b3129bb376a584aad917b502
Ivan Khomyakov <khaxis@gmail.com>
2019-05-30 16:35:13 +0300

    update frontend and docker compose

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,4 +6,4 @@ PLYNX_IMAGES="base backend master worker test"
 
 source ./scripts/build_images.sh
 
-docker-compose -f ./docker/compose-integration/docker-compose.yml up --abort-on-container-exit --scale workers=5
+docker-compose -f ./docker/compose-integration/docker-compose.yml up --abort-on-container-exit --scale workers=5 --scale frontend=0
