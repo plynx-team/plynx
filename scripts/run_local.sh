285b66a4a7e9dfd4f544d025e82269b067834636
Ivan Khomyakov <khaxis@Ivans-MacBook-Pro.local>
2019-06-16 10:56:10 -0700

    remove unused scripts

--- scripts/run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-PLYNX_IMAGES="base backend master worker ui"
-
-source ./scripts/build_images.sh
-
-docker-compose -f ./docker-compose.yml up --abort-on-container-exit --scale workers=5 --scale test=0
