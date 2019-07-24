335b1f3bce9088b25c25a6bf40d5f66af03b3302
Ivan Khomyakov <khaxis@gmail.com>
2019-07-24 15:42:34 -0400

    handle errors in get master state

--- plynx/web/state.py
+++ plynx/web/state.py
@@ -1,9 +1,10 @@
 from plynx.db.service_state import get_master_state
 from plynx.utils.common import JSONEncoder
-from plynx.web.common import app, requires_auth, make_fail_response
+from plynx.web.common import app, requires_auth, make_fail_response, handle_errors
 
 
 @app.route('/plynx/api/v0/master_state', methods=['GET'])
+@handle_errors
 @requires_auth
 def master_state():
     try:
