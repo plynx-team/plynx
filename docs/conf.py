6a964bccd806be99e5acaa5a11a7026991df12a2
Ivan Khomyakov <khaxis@gmail.com>
2020-02-26 21:05:30 -0500

    circleci fix

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ project = 'Plynx'
 #
 # The short X.Y version.
 # version = '1.0.0'
-version = '1.0.0b'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags.
 # release = '1.0.0'
 release = '1.0.0 beta'
--- plynx/__init__.py
+++ plynx/__init__.py
@@ -4,4 +4,4 @@ PLynx
 Interactive, Scalable, Shareable and Reproducible Workflow Orchestration framework
 """
 
-__version__ = '1.0.0b'
+__version__ = '1.0.0'
