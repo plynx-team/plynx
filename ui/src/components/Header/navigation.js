88964f36ac1830bce9c95bc03366bc391d286dde
Ivan Khomyakov <khaxis@gmail.com>
2020-05-09 22:40:39 -0400

    comment out groups in ui

--- ui/src/components/Header/navigation.js
+++ ui/src/components/Header/navigation.js
@@ -25,8 +25,8 @@ class Navigation extends Component {
             {this.state.refreshTokenExists &&
               <NavLink to={`/${VIRTUAL_COLLECTIONS.OPERATIONS}`} className="Item">Operations</NavLink>
             }
-            {this.state.refreshTokenExists &&
-              <NavLink to={`/${VIRTUAL_COLLECTIONS.GROUPS}`} className="Item">Groups</NavLink>
+            {/*this.state.refreshTokenExists &&
+              <NavLink to={`/${VIRTUAL_COLLECTIONS.GROUPS}`} className="Item">Groups</NavLink>*/
             }
             {this.state.refreshTokenExists &&
               <NavLink to={`/${VIRTUAL_COLLECTIONS.WORKFLOWS}`} className="Item">Workflows</NavLink>
